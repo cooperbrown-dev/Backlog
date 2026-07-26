604142164b7025e528aef46022d4d7ec3231033f
Cooper Brown <cooperbrown.dev@gmail.com>
2026-07-26 14:03:55 -0500

    Added manager layer

---
+++ Backlog.Api/Managers/BacklogManager.cs
@@ -0,0 +1,10 @@
+using Backlog.Api.Accessors;
+using Backlog.Api.Dtos;
+namespace Backlog.Api.Managers;
+
+public class BacklogManager(IBacklogAccessor accessor) : IBacklogManager
+{
+    public Task<List<BacklogItemDto>> GetItemsAsync() => accessor.GetAllAsync();
+    public Task<BacklogItemDto> AddItemAsync(CreateBacklogItemRequest request) => accessor.CreateAsync(request);
+    public Task<BacklogItemDto?> UpdateItemAsync(Guid id, UpdateBacklogItemRequest request) => accessor.UpdateAsync(id, request);
+}
---
+++ Backlog.Api/Managers/IBacklogManager.cs
@@ -0,0 +1,9 @@
+using Backlog.Api.Dtos;
+namespace Backlog.Api.Managers;
+
+public interface IBacklogManager
+{
+    Task<List<BacklogItemDto>> GetItemsAsync();
+    Task<BacklogItemDto> AddItemAsync(CreateBacklogItemRequest request);
+    Task<BacklogItemDto?> UpdateItemAsync(Guid id, UpdateBacklogItemRequest request);
+}
